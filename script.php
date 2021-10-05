76837d4aae4bb6bae12c15ea9e5035e23e025fb5
BinGitMaker <dbinoisdevpro@gmail.com>
2021-10-05 18:11:24 +0200

    ajouts fichiers

---
+++ script.php
@@ -0,0 +1,40 @@
+<?php
+
+// ## Instructions
+
+// 1. Dans le fichier `public/script.php`, écrire une fonction `makeMenu()` qui accepte deux paramètres :
+// - un tableau multi-dimensionnel contenant des noms de pages associés à leurs fichiers 
+// (ex : [["name" => "Home", "file" => "/index.php"], ["name" => "About us", "file" => "/about.php"]])
+// - une chaîne de caractères (vide par défaut)
+// 2. La fonction doit retourner le code HTML d'une liste <ul> contenant chaque élement du tableau inscrits dans une balise <a> dont l'attribut href pointera vers le fichier. Le texte du lien affichera le nom. 
+// 3. Si le champ `file` d'un des éléments de la liste correspond à la chaîne passée en deuxième paramètre de la fonction, alors ajouter la classe css "active" au lien &lt;a&gt;.
+
+
+$pages = [
+    [
+        "name" => "Home", 
+        "file" => "/index.php"
+    ], 
+    [
+        "name" => "About us", 
+        "file" => "/about.php"
+    ],
+    [
+        "name" => "Contact", 
+        "file" => "/contact.php"
+    ]
+];
+
+//<li><a class='active' href='/index.php'>Home</a></li>
+function makeMenu(array $pages, string $current = '')
+{
+    $html = "<ul>";
+    foreach ($pages as $page) {
+        $html .= "<li><a";
+        $html .= $current === $page['file'] ? " class='active'" : "";
+        $html .= " href='". $page['file'] . "'>" . $page['name'] . "</a></li>";
+    }
+
+    $html .= "</ul>";
+    return $html;
+}
